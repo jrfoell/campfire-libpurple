f5e4a2be5b186c40ae9c064f6b1990dcb71e344e
Jake Foell <jfoell@gmail.com>
2012-03-02 00:02:25 -0600

    trigger off callback only

--- http.c
+++ http.c
@@ -407,32 +407,21 @@ void campfire_ssl_handler(CampfireConn *campfire, PurpleSslConnection *gsc, Purp
 }
 
 /* prototype */
-static gboolean campfire_ssl_connect(CampfireConn *campfire, PurpleInputCondition cond, gboolean from_connection_callback);
+static void campfire_ssl_connect(CampfireConn *campfire, PurpleInputCondition cond, gboolean from_connection_callback);
 static void campfire_ssl_connect_cb(CampfireConn *campfire, PurpleInputCondition cond)
 {
 	campfire_ssl_connect(campfire, cond, TRUE);
 }
 
-static gboolean campfire_ssl_connect(CampfireConn *campfire, PurpleInputCondition cond, gboolean from_connection_callback)
+static void campfire_ssl_connect(CampfireConn *campfire, PurpleInputCondition cond, gboolean from_connection_callback)
 {
 	GList *first = NULL;
 	CampfireSslTransaction *xaction = NULL;
-	static gboolean connecting = FALSE;
-
-	if (from_connection_callback)
-	{
-		connecting = FALSE;
-	}
-
-	if (connecting)
-	{
-		return TRUE;
-	}
 
 	purple_debug_info("campfire", "%s\n", __FUNCTION__);
 	if(!campfire)
 	{
-		return FALSE;
+		return;
 	}
 	else
 	{
@@ -441,7 +430,7 @@ static gboolean campfire_ssl_connect(CampfireConn *campfire, PurpleInputConditio
 
 	if(!first)
 	{
-		return FALSE;
+		return;
 	}
 	else
 	{
@@ -450,13 +439,12 @@ static gboolean campfire_ssl_connect(CampfireConn *campfire, PurpleInputConditio
 
 	if(!xaction)
 	{
-		return FALSE;
+		return;
 	}
 
 	if(!campfire->gsc)
 	{
 		purple_debug_info("campfire", "new ssl connection\n");
-		connecting = TRUE;
 		campfire->gsc = purple_ssl_connect(campfire->account,
 		                                   campfire->hostname,
 		                                   443,
@@ -468,7 +456,7 @@ static gboolean campfire_ssl_connect(CampfireConn *campfire, PurpleInputConditio
 	else
 	{
 		purple_debug_info("campfire", "previous ssl connection\n");
-		if(g_list_length(campfire->queue) == 1)
+		if (from_connection_callback)
 		{
 			/* campfire_ssl_handler is the ONLY input handler we EVER use
 			 * So... if there is already an input handler present (inpa > 0),
@@ -484,18 +472,15 @@ static gboolean campfire_ssl_connect(CampfireConn *campfire, PurpleInputConditio
 			purple_ssl_write(campfire->gsc, xaction->http_request->str, xaction->http_request->len);
 		}
 	}
-	return FALSE;
+	return;
 }
 
 void campfire_queue_xaction(CampfireConn *campfire, CampfireSslTransaction *xaction, PurpleInputCondition cond)
 {
-	gboolean trying_to_connect;
 	gboolean from_callback = FALSE;
 	purple_debug_info("campfire", "%s input condition: %i\n", __FUNCTION__, cond);
 	campfire->queue = g_list_append(campfire->queue, xaction);
 	purple_debug_info("campfire", "queue length %d\n", g_list_length(campfire->queue));
-	do
-	{
-		trying_to_connect = campfire_ssl_connect(campfire, cond, from_callback);
-	} while (trying_to_connect);
+	campfire_ssl_connect(campfire, cond, from_callback);
 }
+
